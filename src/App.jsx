b9286380940c28cbbb3520b0ac73af789bdb68f4
Anthony Chaiditya <achaiditya@gmail.com>
2024-03-05 12:34:38 -0500

    updated app.jsx to render signup/login

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
 import React, { useState } from 'react';
 import './App.css';
-// import LoginForm from './components/LoginForm/LoginForm.jsx';
-// import RegisterForm from './components/RegisterForm/RegisterForm.jsx';
-import HomePage from './components/HomePage.jsx';
+import LoginForm from './components/LoginForm/LoginForm.jsx';
+import RegisterForm from './components/RegisterForm/RegisterForm.jsx';
+// import HomePage from './components/HomePage.jsx';
 
 function App() {
-  // const [showLoginForm, setShowLoginForm] = useState(true);
+  const [showLoginForm, setShowLoginForm] = useState(true);
 
-  // const toggleForm = () => {
-  //   setShowLoginForm(!showLoginForm);
-  // };
+  const toggleForm = () => {
+    setShowLoginForm(!showLoginForm);
+  };
 
   return (
-    // <div className="App">
-    //   {showLoginForm ? (
-    //     <LoginForm onToggleForm={toggleForm} />
-    //   ) : (
-    //     <RegisterForm onToggleForm={toggleForm} />
-    //   )}
-    // </div>
-    <div>
-      <HomePage />
+    <div className="App">
+      {showLoginForm ? (
+        <LoginForm onToggleForm={toggleForm} />
+      ) : (
+        <RegisterForm onToggleForm={toggleForm} />
+      )}
     </div>
+    // <div>
+    //   <HomePage />
+    // </div>
 
   );
 }
