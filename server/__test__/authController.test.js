cbf039db6a3444dfb8a0307662846b0ad726e687
kelsIam <kellsy.nl@gmail.com>
2024-03-05 10:33:53 -0700

    fixed conflicts

---
+++ server/__test__/authController.test.js
@@ -0,0 +1,28 @@
+// import { describe, test, expect } from "vitest";
+// import authController from "../authController.js";
+
+// test('should return a token on successful login', () => {
+//   expect(authController.response).toBe(200)
+// })
+
+// describe("POST /", () => {
+//   test("should return token on successful login", () => {
+//     const response = response(app)
+//       .post('/')
+//       .send({
+//         user: 'spaceCader',
+//         pass: 'ilovespace'
+//       })
+//       .expect(200);
+//     expect(response.body).toHaveProperty('token');
+//   });
+//   test('should return 401 Unauthorized for invalid credentials',  () => {
+//      request(app)
+//       .post('/')
+//       .send({
+//         user: 'test',
+//         pass: 'incorrect'
+//       })
+//       .expect(401);
+//   });
+// });
