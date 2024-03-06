d4e5302b9ea576a171170ffa5195df130b610054
kelsIam <kellsy.nl@gmail.com>
2024-03-06 12:09:45 -0700

    added browserRouter and Link

--- server/authController.js
+++ server/authController.js
@@ -58,9 +58,9 @@ authController.verifyUser = (req, res, next) => {
 	const { cookies } = req;
 	//destructure token from req.cookies?
 	console.log('cookie: ',cookies);
-	if (!cookies['token']) {
-		return res.send('You must be signed in to view this page');
-	}
+	// if (!cookies['token']) {
+	// 	return res.send('You must be signed in to view this page');
+	// }
 	return next();
 };
 
